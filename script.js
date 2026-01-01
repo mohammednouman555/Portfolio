9d3c02f479cb08e980e0d26e3079bbf1b51fee93
Mohammed Nouman <mohammednouman555@gmail.com>
2026-01-01 16:22:30 +0530

    edited scruipt.js

--- script.js
+++ script.js
@@ -1,37 +1,44 @@
-document.getElementById("contactForm").addEventListener("submit", async function (e) {
-    e.preventDefault();
+const form = document.getElementId("ContactForm");
+cont responseMsg = document.getElementById("responseMsg");
 
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
-    const responseMsg = document.getElementById("responseMsg");
+from.addEventListener("submit", async function (e) {
+    e.preventDefault();
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim;
+    const message = document.getElementById("message").value.trim;
+    const button = form.querySelector("button");
+    if (!name || !email || !message) {
+        responseMsg.style.color = "red";
+        responseMsg.innerText = "Please fill all fields.";
+        return;
+        }
+    button.disabled = true;
+    button.innerText = "Sending...";
+    responseMsg.innerText = "";
 
     try {
-        const response = await fetch("https://portfolio-backend-rgbj.onrender.com/contact", {
+    const res = await fetch("https://portfolio-backend-rgbj.onrender.com/contact", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({
-                name: name,
-                email: email,
-                message: message
-            })
+            body: JSON.stringify({name,email,message})
         });
-
-        const result = await response.json();
-
-        if (response.ok) {
+        const data = await res.json();
+        if (res.ok) {
             responseMsg.style.color = "green";
             responseMsg.innerText = "Message sent successfully!";
-            document.getElementById("contactForm").reset();
-        } else {
+            form.reset();
+        }
+        else {
             responseMsg.style.color = "red";
             responseMsg.innerText = "Failed to send message.";
         }
 
     } catch (error) {
         responseMsg.style.color = "red";
-        responseMsg.innerText = "Server error. Try again later.";
+        responseMsg.innerText = "Network error. Try again later.";
     }
+    button.disabled = false;
+    button.innerText = "Send Message";
 });
